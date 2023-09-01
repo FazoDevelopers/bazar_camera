6c4f3512f23481ec52dda48c9d79e73fb2965ecb
javohirtech <javohirtech@gmail.com>
2023-09-01 23:30:40 +0500

    add sound when detected and human detected signal

--- src/components/MapCamera/NewMap.jsx
+++ src/components/MapCamera/NewMap.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   MapContainer,
   Marker,
@@ -8,11 +8,30 @@ import {
 } from "react-leaflet";
 import L from "leaflet";
 import "./style.css";
+
 function SetViewOnClick({ coords, zoomCustom }) {
   const map = useMap();
-  map.flyTo(coords, zoomCustom);
+
+  useEffect(() => {
+    if (coords && coords.length === 2) {
+      setTimeout(() => {
+        map.flyTo(coords, zoomCustom);
+      }, 100);
+    }
+  }, [coords, zoomCustom, map]);
 
   return null;
+}
+
+function ZoomButton({ position, zoomLevel, onClick }) {
+  return (
+    <button
+      onClick={() => onClick(position, zoomLevel)}
+      style={{ backgroundColor: position.humanDetected ? "red" : "initial" }}
+    >
+      Zoom to {position.name}
+    </button>
+  );
 }
 
 function NewMap() {
@@ -21,64 +40,123 @@ function NewMap() {
   ]);
   const [zoomCustom, setZoom] = useState(12);
 
-  // Assuming position and other variables are defined here or passed as props
-  const position = {
-    cam_id: "some_id",
-    location: [40.99681833333333, 71.64040666666666],
-    name: "Some Name",
-    address: "Some Address",
-  };
-  const detectedAdress = "Some Address"; // Example value
-  const redMarker = "../../../public/marker.png"; // Example value
-  const marker = "../../../public/marker.png"; // Example value
+  const positions = [
+    {
+      cam_id: "id_1",
+      location: [40.99681833333333, 71.64040666666666],
+      name: "Location 1",
+      address: "Address 1",
+      photo: "https://picsum.photos/id/100/50/50",
+      humanDetected: true,
+    },
+    {
+      cam_id: "id_2",
+      location: [41.09681833333333, 71.74040666666666],
+      name: "Location 2",
+      address: "Address 2",
+      photo: "https://picsum.photos/id/101/50/50",
+      humanDetected: false,
+    },
+    {
+      cam_id: "id_3",
+      location: [41.19681833333333, 71.84040666666666],
+      name: "Location 3",
+      address: "Address 3",
+      photo: "https://picsum.photos/id/102/50/50",
+      humanDetected: true,
+    },
+    {
+      cam_id: "id_4",
+      location: [41.29681833333333, 71.94040666666666],
+      name: "Location 4",
+      address: "Address 4",
+      photo: "https://picsum.photos/id/103/50/50",
+      humanDetected: false,
+    },
+    {
+      cam_id: "id_5",
+      location: [41.39681833333333, 72.04040666666666],
+      name: "Location 5",
+      address: "Address 5",
+      photo: "https://picsum.photos/id/104/50/50",
+      humanDetected: true,
+    },
+  ];
 
-  const getJsonLocation = (location) => {
-    // Your logic to convert location to required format
-    return location;
+  const speak = (message) => {
+    if ("speechSynthesis" in window) {
+      const speech = new SpeechSynthesisUtterance(message);
+      speech.rate = 0.9;
+      window.speechSynthesis.speak(speech);
+    } else {
+      console.warn("Your browser doesn't support speech synthesis.");
+    }
   };
 
-  const showInfoModal = (position) => {
-    // setLocationInfo(position); // Assuming this function is defined elsewhere
-    setCenterPositions(position.location);
-    setZoom(15);
+  useEffect(() => {
+    positions.forEach((position) => {
+      if (position.humanDetected) {
+        const message = `${position.address} aniqlandi`;
+        speak(message);
+      }
+    });
+  }, []);
+
+  const handleZoomButtonClick = (position, zoomLevel) => {
+    if (position && position.location && position.location.length === 2) {
+      setCenterPositions(position.location);
+      setZoom(zoomLevel);
+    }
   };
 
   return (
-    <MapContainer
-      center={centerPositions}
-      zoom={zoomCustom}
-      scrollWheelZoom={true}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker
-        key={position.cam_id}
-        position={getJsonLocation(position.location)}
-        icon={
-          new L.Icon({
-            iconUrl: detectedAdress === position.address ? redMarker : marker,
-            iconSize: [40, 40],
-            iconAnchor: [20, 40],
-            popupAnchor: [0, -40],
-          })
-        }
-        eventHandlers={{
-          click: () => {
-            showInfoModal(position);
-          },
-        }}
+    <div>
+      {positions.map((position) => (
+        <ZoomButton
+          key={position.cam_id}
+          position={position}
+          zoomLevel={15}
+          onClick={handleZoomButtonClick}
+        />
+      ))}
+      <MapContainer
+        center={centerPositions}
+        zoom={zoomCustom}
+        scrollWheelZoom={true}
       >
-        <Tooltip permanent>
-            <div>
-                <h1>{position.name}</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, consequuntur consequatur odit enim libero illo! Quae rerum tenetur architecto repellendus perferendis, quasi expedita eos nulla quia pariatur soluta fugiat consectetur!</p>
-            </div>
-        </Tooltip>
-      </Marker>
-      <SetViewOnClick coords={centerPositions} zoomCustom={zoomCustom} />
-    </MapContainer>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {positions.map((position) => (
+          <Marker
+            key={position.cam_id}
+            position={position.location}
+            icon={
+              new L.DivIcon({
+                className: position.humanDetected ? "marker-icon" : "",
+                html: `<img src="${position.photo}" alt="${position.name}" style="width: 50px; height: 50px;" />`,
+                iconSize: [50, 50],
+                iconAnchor: [25, 25],
+                popupAnchor: [0, -25],
+              })
+            }
+          >
+            <Tooltip permanent>
+              <div>
+                <img
+                  src={position.photo}
+                  alt={position.name}
+                  style={{ width: "50px", height: "50px" }}
+                />
+                <p>{position.name}</p>
+              </div>
+            </Tooltip>
+          </Marker>
+        ))}
+        <SetViewOnClick coords={centerPositions} zoomCustom={zoomCustom} />
+      </MapContainer>
+    </div>
   );
 }
 
